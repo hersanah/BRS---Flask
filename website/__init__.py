f3159ae8e264fdb4c64889fac5bf7f91e9d6704d
hersanah <hadeyanju1804@gmail.com>
2024-06-18 09:16:45 +0100

    registering views and auth blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'CSC520 Group 6'
 
+    #registering blueprints
+    #------------------------
+    #importing blueprints from other files
+    from .views import views
+    from .auth import auth
+
+    #registering
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
