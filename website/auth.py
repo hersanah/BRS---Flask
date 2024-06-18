553166ffca06dcd4c1214e23cfe52848a6d99801
hersanah <hadeyanju1804@gmail.com>
2024-06-18 09:26:01 +0100

    create routes and views

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
 from flask import Blueprint
 
-auth = Blueprint("auth", __name__)
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Log In</p>"
+
+@auth.route('/signup')
+def signup():
+    return "<p>Sign Up</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Log Out</p>"
