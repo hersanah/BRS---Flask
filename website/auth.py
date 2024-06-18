90cf7432fc00b20a1738233ebe365906377c9d71
hersanah <hadeyanju1804@gmail.com>
2024-06-18 22:55:37 +0100

    make changes to frontend packages

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,44 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Log In</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+    return render_template("signin.html")
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be longer than 4 characters', category='error')
+
+        elif len(first_name) < 2:
+            flash('Invalid First Name', category='error')
+
+        elif len(last_name) < 2:
+            flash('Invalid Last Name', category='error')
+
+        elif len(password1) < 8:
+            flash('Password is too short', category='error')
+
+        elif password1 != password2:
+            flash('Password don\'t match', category='error')
+
+        else:
+            #add user to database
+            flash('Account Created!', category='success')
+
+
+    return render_template("signup.html")
 
 @auth.route('/logout')
 def logout():
--- website/views.py
+++ website/views.py
@@ -5,4 +5,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html", user=current_user)
+    return render_template("index.html", user=current_user)
