a10f6b878b34254a82c33a8d4b75639ea57b76cd
hersanah <hadeyanju1804@gmail.com>
2024-06-20 14:03:57 +0100

    Work on fixing id returned from mysql database

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import views, mysql
+from . import mysql
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
+@auth.route('/login/<existence>', methods=['GET', 'POST'])
+def login(existence):
+
+    if existence == 1:
+        flash('Account already exists', category='error')
+
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -19,17 +23,35 @@ def login():
         cursor.close()
 
         return render_template("home.html", data=user)
-    return render_template("signin.html")
+    return render_template("signin.html", existence=0)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        last_name = request.form.get('lastName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
 
+        # Get form data
+        userDetails = request.form
+        
+        email = userDetails['email']
+        first_name = userDetails['firstName']
+        last_name = userDetails['lastName']
+        password1 = userDetails['password1']
+        password2 = userDetails['password2']
+
+        # Checking if user exists
+        cur1 = mysql.connection.cursor()
+        cur1.execute('''
+            select id from users
+            where email= %s
+        ''', [email])
+        user = cur1.fetchone()
+        cur1.close()
+
+        if user:
+            #  reidrect existing user to log in page
+            return redirect(url_for('auth.login', existence=1))
+            
+        # Checking form data
         if len(email) < 4:
             flash('Email must be longer than 4 characters', category='error')
 
@@ -47,19 +69,27 @@ def signup():
 
         else:
             password = generate_password_hash(password1, method='scrypt')
+
             #add user to database
-            cursor = mysql.connection.cursor()
-            cursor.execute(
-                '''INSERT INTO users VALUES (%s, %s, %s, %s)''',
-                (first_name, last_name, email, password)
+            cur2 = mysql.connection.cursor()
+            cur2.execute(
+                '''INSERT INTO users(email, first_name, last_name, password)
+                VALUES (%s, %s, %s, %s)''',
+                (email, first_name, last_name, password)
             )
             mysql.connection.commit()
-            cursor.close()
+            cur2.execute('''
+            select id from users
+            where email= %s
+            ''', [email])
+            userid = cur2.fetchone()
+            cur2.close()
             flash('Account Created!', category='success')
-            return render_template("index.html")
+            print(userid[0])
+            return redirect(url_for('views.select_gender', user=userid))
 
     return render_template("signup.html")
 
-@auth.route('/logout')
-def logout():
-    return "<p>Log Out</p>"
+# @auth.route('/logout')
+# def logout():
+#     return "<p>Log Out</p>"
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Flask
 from flask_login import current_user
 
 views = Blueprint('views', __name__)
@@ -6,3 +6,9 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("index.html", user=current_user)
+
+
+@views.route('/user-gender/<user>')
+def select_gender(user):
+
+    return render_template("gender-selection.html", data=user, first_name="Test")
